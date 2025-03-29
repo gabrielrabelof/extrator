f2535a4f38638df3ef6d8f6223cb02514b3eb711
Gabriel Rabelo <rabelo.g14@gmail.com>
2025-03-29 15:55:05 -0300

    configurações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/extrator",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
