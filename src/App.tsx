36760c5c0d261e2cea9e5fa6333fe3e65b0c8b90
Gabriel Rabelo <rabelo.g14@gmail.com>
2025-03-31 10:21:33 -0300

    Ajuste HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import QuestionarioPage from "./pages/questionario/[id]";
 import NotFound from "./pages/NotFound";
@@ -14,12 +15,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner position="top-center" />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/questionario/:id" element={<QuestionarioPage />} />
           <Route path="/" element={<Index />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
